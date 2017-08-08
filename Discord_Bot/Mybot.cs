93c0ec7c96864b6f773d3e33217f98a703dc3dda
johnrvanwie <johnrvanwie@gmail.com>
2017-08-07 21:37:14 -0700

    Add project files.

---
+++ Discord_Bot/Mybot.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    class Mybot
+    {
+        IDiscordClient discord;
+
+
+        public Mybot()
+        {
+            discord = new IDiscordClient(x =>
+
+           {
+               x.LogLevel = LogSeverity.Info;
+               x.LogHandler = Log;
+           });
+
+            discord.ExecuteAndWait(async () =>
+
+
+            {
+
+
+
+            }  };
+        }
+
+       private void Log(object sender, LogMessage e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+    }
+}
---
+++ Discord_Bot/Program.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    class Program
+    {
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private IServiceProvider services;
+
+        string token = "";
+
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+
+        {
+            client = new DiscordSocketClient();
+
+            commands = new CommandService();
+
+            services = new ServiceCollection().BuildServiceProvider();
+
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            client.Log += Log;
+
+            await Task.Delay(-1);
+
+              
+        }
+
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+
+            await commands.AddModuleAsync(Assembly.GetEntryAssembly());
+        }
+        
+        //{
+        //    Mybot bot = new Mybot();
+
+        //}
+    }
+}
+// https://discordapp.com/oauth2/authorize?client_id=336918100127645706&scope=bot&permissions=0
+
+//336918100127645706
