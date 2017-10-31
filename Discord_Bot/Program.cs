2e4f3d04e0022494900335cb555c82f069d3ab44
garfator <johnrvanwie@gmail.com>
2017-10-31 15:16:31 -0700

    Test

--- Discord_Bot/Program.cs
+++ Discord_Bot/Program.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Discord_Bot
-{//Why does nothing work? No NuGetpackage?
+{//Why does nothing work? No NuGetpackage? Testing WILL THIS WORK NOW?
     class Program
     {
         private CommandService commands;
